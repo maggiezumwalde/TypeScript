61cff91a16ae24963aaac799607311acaa61115e
max-student@outlook.com <max-student@outlook.com>
2019-09-11 13:42:21 -0400

    added comment

--- hello-world/ts/hello-world.ts
+++ hello-world/ts/hello-world.ts
@@ -3,3 +3,7 @@ let hhw:string = "Hello, world";
 let Name:string = "student";
 
 console.log(hhw, Name);
+
+let a: number = 1;
+let b: number = 2;
+console.log(a + b);
--- hello-world/ts/hello-world1.ts
+++
@@ -1,3 +0,0 @@
-let a: number = 1;
-let b: number = 2;
-console.log(a + b);
